0a5bffdd1ab52cb7f345d0c56797bcae14382abb
jacob <jacob.williams33@gmail.com>
2022-04-02 13:07:22 -0600

    Initial commit

---
+++ PackageClass.py
@@ -0,0 +1,9 @@
+class package:
+    def __int__(self, packageID, address, city, state, zip, deadline, weight, status):
+        self.packageID = packageID
+        self.address = address
+        self.city = city
+        self.zip = zip
+        self.deadline = deadline
+        self.weight = weight
+        self.status = status
---
+++ TruckClass.py
@@ -0,0 +1,9 @@
+class truck:
+    def __init__(self, truckID, speed, distance, capacity, currentLoad, time):
+        self.truckID = truckID
+        self.speed = speed
+        self.distance = distance
+        self.capacity = capacity
+        self.currentLoad = currentLoad
+        self.time = time
+        
